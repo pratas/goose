fad84066ea6982a11d5027822997aa9586e63cb6
pratas <pratas@ua.pt>
2015-10-15 15:27:40 +0100

    download scripts

---
+++ GetAll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. GetOrangutanParse.sh  
+. GetGorillaParse.sh  
+. GetChimpParse.sh
+. GetHumanParse.sh
+#. GetHumanCHMParse.sh
+#. GetNeandertal.sh
---
+++ GetNeandertalParse.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# http://cdna.eva.mpg.de/neandertal/altai/AltaiNeandertal/bam/
