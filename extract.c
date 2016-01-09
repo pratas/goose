0661cf8e150b46f949879911cf5e00cdf40ce9aa
pratas <pratas@ua.pt>
2016-01-09 22:42:30 +0000

    extractChars

--- extract.c
+++ extract.c
@@ -6,53 +6,37 @@
 #include "misc.h"
 
 int main(int argc, char *argv[]){
-  FILE *IN = NULL, *OUT = NULL;
   uint32_t n;
   uint64_t cnt = 0, init = 0, end = 0;
   char c;
 
   if(argc < 9){
-    fprintf(stderr, "USAGE:\n%s -i IN_FILE -o OUT_FILE -pS FIRST_POSITIO"
-    "N_NUMBER -pL LAST_POSITION_NUMBER\nCOMMON USAGE:\nDNAExt -i input -o ou"
-    "tput -pS 1000 -pL 1000000\n", argv[0]);
+    fprintf(stderr, "USAGE:\n%s -s FIRST_POSITION_NUMBER -l "
+    "LAST_POSITION_NUMBER < inFile > outFile\n"
+    "COMMON USAGE:\n%s -s 1 -l 1000000 < ch3 > ch3_1M\n", 
+    argv[0], argv[0]);
     return EXIT_SUCCESS;
     }
 
-  fprintf(stderr, "[>] Starting Extract ...\n");
-
-  for(n = 1 ; n < argc ; n++)
-    if(strcmp("-i", argv[n]) == 0){
-      IN = Fopen(argv[n+1], "r");
-      break;
-      }
-
-  for(n = 1 ; n < argc ; n++)
-    if(strcmp("-o", argv[n]) == 0){
-      OUT = Fopen(argv[n+1], "w");
-      break;
-      }
-
   for(n = 1; n < argc; n++)
-    if(strcmp("-pS",argv[n]) == 0){	
+    if(strcmp("-s", argv[n]) == 0){	
       init = atol(argv[n+1]);
       break;
       }
 
   for(n = 1; n < argc; n++)
-    if(strcmp("-pL",argv[n]) == 0){      
+    if(strcmp("-l", argv[n]) == 0){      
       end = atol(argv[n+1]);
       break;
       }
 
   do{
-    c = fgetc(IN);
+    c = fgetc(stdin);
     if(init <= cnt && end > cnt)
-      fprintf(OUT, "%c", c);		
+      fprintf(stdout, "%c", c);		
     if(cnt++ >= end) break;
     }
   while(c != EOF);
 
-  fprintf(stderr, "[>] Done!\n");
-  fclose(IN); fclose(OUT);
   return EXIT_SUCCESS;
   }
