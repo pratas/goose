dcd9cf69ebb952c82bd9b9c9f65234294f0443ae
pratas <pratas@ua.pt>
2016-01-10 00:22:57 +0000

    gooseMain

---
+++ goose.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "defs.h"
+
+int main(int argc, char *argv[]){
+  fprintf(stderr, "GOOSE, 2015-2016 [UNIVERSITY OF AVEIRO],"
+  "a framework for DNA sequences manipulation and analysis.\n"
+  "Goose programs:\n"
+  "  [+] goose-char2line            \n"            
+  "  [+] goose-filter               \n"
+  "  [+] goose-randfastaextrachars  \n"
+  "  [+] goose-extract              \n"
+  "  [+] goose-findnpos             \n"
+  "  [+] goose-randfastqextrachars  \n"
+  "  [+] goose-fasta2seq            \n"
+  "  [+] goose-genrandomdna         \n"
+  "  [+] goose-randseqextrachars    \n"
+  "  [+] goose-fastaextract         \n"
+  "  [+] goose-info                 \n"
+  "  [+] goose-reverse              \n"
+  "  [+] goose-fastq2fasta          \n"
+  "  [+] goose-min                  \n"
+  "  [+] goose-searchphash          \n"
+  "  [+] goose-fastq2mfasta         \n"
+  "  [+] goose-minus                \n"
+  "  [+] goose-seq2fasta            \n"
+  "  [+] goose-fastqclustreads      \n"
+  "  [+] goose-mutatedna            \n"
+  "  [+] goose-seq2fastq            \n"
+  "  [+] goose-fastqpack            \n"
+  "  [+] goose-period               \n"
+  "  [+] goose-wsearch              \n"
+  "  [+] goose-fastqunpack          \n"
+  "  [+] goose-permuteseqbyblocks   \n");
+  return EXIT_SUCCESS;
+  }
+
