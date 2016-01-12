f20754c302fd7a93e1960e0e31a67543c3369470
pratas <pratas@ua.pt>
2016-01-12 15:00:39 +0000

    addOptionalHeader

--- src/Seq2Fasta.c
+++ src/Seq2Fasta.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "defs.h"
 #include "misc.h"
+#include "args.h"
+#include "mem.h"
 
 #define BREAKER 100
 
 int main(int argc, char *argv[]){
-  uint32_t k, i, pos = 0;
+  uint32_t n, k, i, pos = 0;
   BUF *B;
 
-  if(argc != 1){
-    fprintf(stderr, "Usage: %s < input.seq > output.fasta\nIt converts a "
-    "genomic sequence to pseudo FASTA file format.\n", argv[0]);
+  if(argc == 0 || argc > 3){
+    fprintf(stderr, "Usage: %s -n <name> < input.seq > output.fasta\nIt "
+    "converts a genomic sequence to pseudo FASTA file format.\n", argv[0]);
     return EXIT_SUCCESS;
     }
 
-  B = CreateBuffer(BUF_SIZE);
+  for(n = 1 ; n < argc ; ++n)
+    if(strcmp(argv[n], "-n") == 0){
+      fprintf(stdout, "> %s\n", argv[n+1]);
+      goto XPS;
+      }
   fprintf(stdout, "> Computed with %s\n", argv[0]);
+   
+  XPS:
+  B = CreateBuffer(BUF_SIZE);
   while((k = fread(B->buf, 1, B->size, stdin)))
     for(i = 0 ; i < k ; ++i){
       putchar(B->buf[i]);
@@ -25,8 +35,8 @@ int main(int argc, char *argv[]){
         pos = 0;
         }
       }
-
   RemoveBuffer(B); 
+
   return EXIT_SUCCESS;
   }
 
