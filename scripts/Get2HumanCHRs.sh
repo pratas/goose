9a703102cf609d0a75f68e82578a7abf5d378c09
pratas <pratas@ua.pt>
2016-04-07 17:22:43 +0100

    wget

--- scripts/Get2HumanCHRs.sh
+++ scripts/Get2HumanCHRs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #=============================================================================
-WGETOP=" --trust-server-names -q ";
-#-----------------------------------------------------------------------------
 echo "Downloading and filtering HS C12 sequences ..."
-wget "$WGETOP" " ftp://ftp.ncbi.nlm.nih.gov/genomes/Homo_sapiens/Assembled_chromosomes/seq/hs_ref_GRCh38.p2_chr12.fa.gz " -O C12.fa.gz;
+wget --trust-server-names -q ftp://ftp.ncbi.nlm.nih.gov/genomes/Homo_sapiens/Assembled_chromosomes/seq/hs_ref_GRCh38.p2_chr12.fa.gz -O C12.fa.gz;
 zcat C12.fa.gz | grep -v ">" | tr -d -c "ACGTN" > C12
 echo "Done!";
 #-----------------------------------------------------------------------------
 echo "Downloading and filtering HS C18 sequences ..."
-wget "$WGETOP" " ftp://ftp.ncbi.nlm.nih.gov/genomes/Homo_sapiens/Assembled_chromosomes/seq/hs_ref_GRCh38.p2_chr18.fa.gz "  -O C18.fa.gz;
+wget --trust-server-names -q ftp://ftp.ncbi.nlm.nih.gov/genomes/Homo_sapiens/Assembled_chromosomes/seq/hs_ref_GRCh38.p2_chr18.fa.gz -O C18.fa.gz;
 zcat C18.fa.gz | grep -v ">" | tr -d -c "ACGTN" > C18
 echo "Done!";
 #=============================================================================
