27a1844e388391f66477e6ca1a7b5a2fd51f6062
pratas <pratas@ua.pt>
2016-04-15 11:56:00 +0100

    reverse file using low memory

---
+++ src/reverselm.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "defs.h"
+#include "misc.h"
+#include "mem.h"
+
+int main(int argc, char *argv[]){
+  FILE *IN = Fopen(argv[argc-1], "r");
+  int64_t i, size;
+
+  if(argc != 2){
+    fprintf(stderr, "Reverse huge file using very low memory.\n");
+    fprintf(stderr, "Usage: %s file\n", argv[0]);
+    return EXIT_SUCCESS;
+    }
+
+  fseeko(IN, 0, SEEK_END);
+  size = ftello(IN);
+    
+  i = 0; 
+  while(i++ < size){
+    fseeko(IN, -i, SEEK_END);
+    printf("%c",fgetc(IN));
+    }
+  printf("\n");
+  fclose(IN);
+
+  return EXIT_SUCCESS;
+  }
+
