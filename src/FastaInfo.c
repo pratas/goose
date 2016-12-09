27c6db82d8fd49ba149509c7d38e7e2e0aa1e343
pratas <pratas@ua.pt>
2016-12-09 13:58:05 +0000

    info for mfasta reads

--- src/FastaInfo.c
+++ src/FastaInfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "defs.h"
 #include "misc.h"
 #include "mem.h"
@@ -18,18 +19,40 @@ int main(int argc, char *argv[]){
 
   B = CreateBuffer(BUF_SIZE);
 
+  uint64_t min_n_bases = UINT_MAX, max_n_bases = 0, cum_n_bases = 0, 
+           i_bases = 0, n_reads = 0;
+
   while((k = fread(B->buf, 1, B->size, stdin)))
     for(i = 0 ; i < k ; ++i){
       s = B->buf[i];
-      if(s == '>'){ header = 1; continue; }
+      if(s == '>'){ 
+        header = 1; 
+        cum_n_bases += i_bases;
+
+        if(i_bases > max_n_bases)
+          max_n_bases = i_bases;        
+
+        if(i_bases < min_n_bases)
+          min_n_bases = i_bases;        
+
+        ++n_reads;
+        i_bases = 0;
+        continue; 
+        }
       if(s == '\n' && header == 1){ header = 0; continue; }
       if(s == '\n') continue;
       if(header == 1) continue;
       if(s < 65 || s > 122) continue;
 
-
-      putchar(s);
+      ++i_bases;
       }
+
+  fprintf(stdout, "Number of reads      : %"PRIu64"\n", n_reads);
+  fprintf(stdout, "Number of bases      : %"PRIu64"\n", cum_n_bases);
+  fprintf(stdout, "MIN of bases in read : %"PRIu64"\n", min_n_bases);
+  fprintf(stdout, "MAX of bases in read : %"PRIu64"\n", max_n_bases);
+  fprintf(stdout, "AVG of bases in read : %.4lf\n", (double) cum_n_bases / 
+  n_reads);
 
   RemoveBuffer(B); 
   return EXIT_SUCCESS;
