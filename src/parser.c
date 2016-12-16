f18815e494539f4d1007564a2537988963817a86
pratas <pratas@ua.pt>
2016-12-16 12:07:45 +0000

    fix error in name func

--- src/parser.c
+++ src/parser.c
@@ -104,6 +104,39 @@ int32_t ParseSym(PARSER *PA, uint8_t sym){
   }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// PARSE SYM
+//
+int32_t ParseSymN(PARSER *PA, uint8_t sym){
+
+  switch(PA->type){
+    // IS A FASTA ">" FILE
+    case 1:
+      switch(sym){
+        case '>':  PA->header = 1; return -1;
+        case '\n': PA->header = 0; return -1;
+        default:   if(PA->header==1) return -1;
+        }
+    break;
+
+    // IS A FASTQ "@" FILE
+    case 2:
+      switch(PA->line){
+        case 0: if(sym == '\n'){ PA->line = 1; PA->dna = 1; } break;
+        case 1: if(sym == '\n'){ PA->line = 2; PA->dna = 0; } break;
+        case 2: if(sym == '\n'){ PA->line = 3; PA->dna = 0; } break;
+        case 3: if(sym == '\n'){ PA->line = 0; PA->dna = 0; } break;
+        }
+      if(PA->dna == 0 || sym == '\n') return -1;
+    break;
+
+    // OTHER (SUCH AS DNA SEQ)
+    default: ; // DO NOTHING
+    }
+
+  return FBasesPolN(sym);
+  }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // PARSE MULTI-FASTA
 //
 int32_t ParseMF(PARSER *PA, uint8_t sym){
