56109381b5eec858bbcf3176c8775b9d13b2ce16
pratas <pratas@ua.pt>
2016-12-23 16:26:49 +0000

    Paint regions working

--- scripts/PaintRegions.sh
+++ scripts/PaintRegions.sh
@@ -3,11 +3,11 @@
 # USAGE: . PaintRegions.sh CACAGAGTTTAACCTTTCTTTTCATAGAGCAGTTAGGAAACA
 #
 #==============================================================================
-GET_GOOSE=1;
-GET_CHESTER=1;
-GET_GECO=1;
-DWD_SEQS=1;
-RUN_GECO=1;
+GET_GOOSE=0;
+GET_CHESTER=0;
+GET_GECO=0;
+DWD_SEQS=0;
+RUN_GECO=0;
 FILTER=1;
 PAINT=1;
 #==============================================================================
@@ -46,26 +46,28 @@ fi
 #==============================================================================
 # DOWNLOAD SEQUENCES
 if [[ "$DWD_SEQS" -eq "1" ]]; then
+  rm -f hs_ref_GRCh38.p7_chr1.fa.gz hs_ref_GRCh38.p7_chr1.fa
   NCBIC="ftp://ftp.ncbi.nlm.nih.gov/genomes/H_sapiens/Assembled_chromosomes";
   echo "$1" > REFERENCE;
   wget $NCBIC/seq/hs_ref_GRCh38.p7_chr1.fa.gz
   gunzip hs_ref_GRCh38.p7_chr1.fa.gz
-  cat hs_ref_GRCh38.p7_chr1.fa | grep -v ">" | tr -d -c "ACGTN" > TARGET;
+  cat hs_ref_GRCh38.p7_chr1.fa | grep -v ">" | tr -d -c "ACGTN" \
+  | ./goose-randseqextrachars > TARGET;
 fi
 #==============================================================================
 # RUN_GECO
 if [[ "$RUN_GECO" -eq "1" ]]; then
-  ./GeCo -e -c 5 -rm 18:100:1:0/0 -r REFERENCE TARGET;
+  ./GeCo -v -e -c 5 -rm 18:100:1:0/0 -r REFERENCE TARGET;
 fi
 #==============================================================================
 # FILTER
 if [[ "$FILTER" -eq "1" ]]; then
-  ./CHESTER-filter -v -t 1.5 TARGET.iae
+  ./goose-real2binthreshold 1.5 < TARGET.iae > TARGET.bin
+  ./CHESTER-filter -v -t 0.9 TARGET.bin
 fi
 #==============================================================================
 # PAINT
 if [[ "$PAINT" -eq "1" ]]; then
-  #
-  #
+  ./CHESTER-visual -e 1 TARGET.bin.seg
 fi
 #==============================================================================
