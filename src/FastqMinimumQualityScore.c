c39439dca6d02b79d1baddb51ff205fc62a7826a
pratas <pratas@ua.pt>
2017-07-26 17:07:05 +0100

    fix

--- src/FastqMinimumQualityScore.c
+++ src/FastqMinimumQualityScore.c
@@ -9,10 +9,12 @@
 #include "args.h"
 
 void PrintArgs(char *name){
-  fprintf(stderr, "Usage: %s [VALUE] < input > output                  \n"
-                  "                                                    \n"
-                  "Discard read with average quality-score below value.\n"
-                  "Value 25 or 30 is commonly used.                    \n",
+  fprintf(stderr, "                                                        \n"
+                  "Usage: %s [VALUE] < input > output                      \n"
+                  "                                                        \n"
+                  "Discard read with average quality-score below value.    \n"
+                  "Value 25 or 30 is commonly used.                        \n"
+                  "                                                        \n",
   name);
   }
 
@@ -24,12 +26,12 @@ int main(int argc, char *argv[]){
   uint32_t seqSize = 0, totalQS = 0, x, n;
   uint64_t okReads = 0, totalReads = 0;
 
-  if(ArgBin(0, argv, argc, "-h") || ArgBin(0, argv, argc, "?")){
+  if(argc < 2 || ArgBin(0, argv, argc, "-h") || ArgBin(0, argv, argc, "?")){
     PrintArgs(argv[0]);
     return EXIT_SUCCESS;
     }
   
-  int min_avg_qs = atoi(argv[2]);
+  int min_avg_qs = atoi(argv[1]);
  
   while(GetRead(stdin, Read)){
 
@@ -46,12 +48,13 @@ int main(int argc, char *argv[]){
     if((totalQS / seqSize) < min_avg_qs) continue;
 
     // PRINT READ
+    fprintf(stdout, "@");
     for(n = 0 ; n < strlen((char *) Read->header1) ; ++n)
       fprintf(stdout, "%c", Read->header1[n]);
-    for(n = 0 ; n <= seqSize ; ++n)
+    for(n = 0 ; n < seqSize ; ++n)
       fprintf(stdout, "%c", Read->bases[n]);
     fprintf(stdout, "\n+\n");
-    for(n = 0 ; n <= seqSize ; ++n)
+    for(n = 0 ; n < seqSize ; ++n)
       fprintf(stdout, "%c", Read->scores[n]);
     fprintf(stdout, "\n");
 
