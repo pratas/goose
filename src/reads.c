029782159f38cc7466b13d2e717bd46b45229d8b
pratas <pratas@ua.pt>
2017-09-29 18:36:35 +0100

    corrOut

--- src/reads.c
+++ src/reads.c
@@ -112,10 +112,10 @@ Read *GetRead(FILE *F, Read *R){
 // PRINT READ
 //
 void PrintRead(Read *R, FILE *F){
-  fprintf(F, "%s\n", R->header1);
-  fprintf(F, "%s\n", R->bases);
-  fprintf(F, "%s\n", R->header2);
-  fprintf(F, "%s\n", R->scores);
+  fprintf(F, "@%s", R->header1);
+  fprintf(F, "%s", R->bases);
+  fprintf(F, "%s", R->header2);
+  fprintf(F, "%s", R->scores);
   }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
