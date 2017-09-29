16c6c985e697f3574cb97cffff8a6c5ea5266589
pratas <pratas@ua.pt>
2017-09-29 18:27:08 +0100

    singletonReadsHandling

--- src/FastqSplit.c
+++ src/FastqSplit.c
@@ -13,7 +13,8 @@ void PrintArgs(char *name){
                   "Usage: %s <output_forward> <output_reverse> < input   \n"
                   "                                                      \n"
                   "It splits Paired End files according to the           \n"
-                  "direction of the strand ('/1' or '/2').               \n"
+                  "direction of the strand ('/1' or '/2'). It writes by  \n"
+                  "default singleton reads as forward stands.            \n"
                   "                                                      \n"
                   "Example: ./%s HN_r1.fq HN_r2.fq < HN.fq               \n"
                   "                                                      \n",
@@ -25,7 +26,7 @@ void PrintArgs(char *name){
 int main(int argc, char *argv[]){
 
   Read      *Read = CreateRead(65536+GUARD, 65535+GUARD);
-  uint64_t  total = 0, forward = 65535, reverse = 0;
+  uint64_t  total = 0, forward = 65535, reverse = 0, singleton = 0;
 
   if(argc < 2 || ArgBin(0, argv, argc, "-h") || ArgBin(0, argv, argc, "?")){
     PrintArgs(argv[0]);
@@ -47,14 +48,15 @@ int main(int argc, char *argv[]){
         ++reverse;   
       break;
       default:
-        fprintf(stderr, "Error: Unknown file type (%d)!\n",
-        (int) Read->header1[strlen((char *) Read->header1)-2]);
+        PrintRead(Read, F);
+        ++singleton;
       }
 
     ++total;
     }
 
   fprintf(stderr, "Total reads     : %"PRIu64"\n", total);
+  fprintf(stderr, "Singleton reads : %"PRIu64"\n", singleton);
   fprintf(stderr, "Forward reads   : %"PRIu64"\n", forward);
   fprintf(stderr, "Reverse reads   : %"PRIu64"\n", reverse);
 
