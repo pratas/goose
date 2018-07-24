a5dd2c8699716ab5eea532e7e6d14be33de4848c
joaorafaelalmeida <joao.rafael.almeida@ua.pt>
2018-07-24 18:52:56 +0100

    Code refactor in geco.c

--- src/geco.c
+++ src/geco.c
@@ -19,8 +19,8 @@
 //////////////////////////////////////////////////////////////////////////////
 // - - - - - - - - - - - - - - C O M P R E S S O R - - - - - - - - - - - - - -
 
-void Compress(Parameters *P, CModel **cModels, uint8_t id, uint32_t 
-refNModels, INF *I){
+void Compress(Parameters *P, CModel **cModels, uint8_t id, uint32_t refNModels, INF *I)
+{
   FILE        *Reader  = Fopen(P->tar[id], "r");
   char        *name    = concatenate(P->tar[id], ".co");
   FILE        *Writter = Fopen(name, "w");
@@ -32,35 +32,39 @@ refNModels, INF *I){
               header = 1, line = 0, dna = 0;
   PModel      **pModel, *MX;
   FloatPModel *PT;
+
   #ifdef PROGRESS
-  uint64_t    i = 0;
+    uint64_t    i = 0;
   #endif
 
   if(P->verbose)
     fprintf(stdout, "Analyzing data and creating models ...\n");
 
   #ifdef ESTIMATE
-  FILE *IAE = NULL;
-  char *IAEName = NULL;
-  if(P->estim == 1){
-    IAEName = concatenate(P->tar[id], ".iae");
-    IAE = Fopen(IAEName, "w");
+    FILE *IAE = NULL;
+    char *IAEName = NULL;
+    if(P->estim == 1)
+    {
+      IAEName = concatenate(P->tar[id], ".iae");
+      IAE = Fopen(IAEName, "w");
     }
   #endif
   
   sym = fgetc(Reader);
-  switch(sym){
+  switch(sym)
+  {
     case '>': type = 1; break;
     case '@': type = 2; break;
     default : type = 0;
-    }
+  }
   rewind(Reader);
 
-  switch(type){
+  switch(type)
+  {
     case 1:  nBases = NDNASymInFasta(Reader); break;
     case 2:  nBases = NDNASymInFastq(Reader); break;
     default: nBases = NDNASyminFile (Reader); break;
-    }
+  }
  
   _bytes_output = 0;
   nSymbols      = NBytesInFile(Reader);
@@ -84,18 +88,21 @@ refNModels, INF *I){
   for(n = 0 ; n < totModels ; ++n)
     cModelWeight[n] = 1.0 / totModels;
 
-  for(n = 0 ; n < P->nModels ; ++n){
-    if(P->model[n].type == TARGET){
+  for(n = 0 ; n < P->nModels ; ++n)
+  {
+    if(P->model[n].type == TARGET)
+    {
       cModels[n] = CreateCModel(P->model[n].ctx, P->model[n].den, 
       P->model[n].ir, TARGET, P->col, P->model[n].edits, P->model[n].eDen);
-      }
     }
+  }
 
-  if(P->verbose){
+  if(P->verbose)
+  {
     fprintf(stdout, "Done!\n");
     fprintf(stdout, "Compressing target sequence %d [bases: %"PRIu64"] ...\n", 
     id + 1, nBases);
-    }
+  }
 
   startoutputtingbits();
   start_encode();
@@ -106,64 +113,73 @@ refNModels, INF *I){
   WriteNBits((int) (P->gamma * 65536), 32, Writter);
   WriteNBits(P->col,                   32, Writter);
   WriteNBits(P->nModels,               16, Writter);
-  for(n = 0 ; n < P->nModels ; ++n){
+
+  for(n = 0 ; n < P->nModels ; ++n)
+  {
     WriteNBits(cModels[n]->ctx,        16, Writter);
     WriteNBits(cModels[n]->alphaDen,   16, Writter);
     WriteNBits(cModels[n]->ir,          1, Writter);
     WriteNBits(cModels[n]->edits,       8, Writter);
     WriteNBits(cModels[n]->SUBS.eDen,  32, Writter);
     WriteNBits(P->model[n].type,        1, Writter);
-    }
+  }
 
   I[id].header = _bytes_output;
 
   while((k = fread(readerBuffer, 1, BUFFER_SIZE, Reader)))
-    for(idxPos = 0 ; idxPos < k ; ++idxPos){
+    for(idxPos = 0 ; idxPos < k ; ++idxPos)
+    {
       #ifdef PROGRESS
-      CalcProgress(nSymbols, ++i);
+        CalcProgress(nSymbols, ++i);
       #endif
 
       sym = readerBuffer[idxPos];
-      if(type == 1){  // IS A FAST[A] FILE
+      if(type == 1)  // IS A FAST[A] FILE
+      {  
         if(sym == '>'){ header = 1; continue; }
         if(sym == '\n' && header == 1){ header = 0; continue; }
         if(sym == '\n') continue;
         if(header == 1) continue;
-        }
-      else if(type == 2){ // IS A FAST[Q] FILE
-        switch(line){
+      }
+      else if(type == 2) // IS A FAST[Q] FILE
+      {  
+        switch(line)
+        {
           case 0: if(sym == '\n'){ line = 1; dna = 1; } break;
           case 1: if(sym == '\n'){ line = 2; dna = 0; } break;
           case 2: if(sym == '\n'){ line = 3; dna = 0; } break;
           case 3: if(sym == '\n'){ line = 0; dna = 0; } break;
-          }
-        if(dna == 0 || sym == '\n') continue;
         }
+        if(dna == 0 || sym == '\n') continue;
+      }
 
       // REMOVE SPECIAL SYMBOLS [WINDOWS TXT ISSUES]
       if(sym < 65 || sym > 122) continue; 
 
       // FINAL FILTERING DNA CONTENT
-      if(sym != 'A' && sym != 'C' && sym != 'G' && sym != 'T'){
+      if(sym != 'A' && sym != 'C' && sym != 'G' && sym != 'T')
+      {
         #ifdef ESTIMATE
-        if(P->estim != 0)
-          fprintf(IAE, "0\n");
+          if(P->estim != 0)
+            fprintf(IAE, "0\n");
         #endif
         continue;
-        }
+      }
 
       symbolBuffer[idx] = sym = DNASymToNum(sym);
       memset((void *)PT->freqs, 0, ALPHABET_SIZE * sizeof(double));
 
       n = 0;
       pos = &symbolBuffer[idx-1];
-      for(cModel = 0 ; cModel < P->nModels ; ++cModel){
+      for(cModel = 0 ; cModel < P->nModels ; ++cModel)
+      {
         GetPModelIdx(pos, cModels[cModel]);
         ComputePModel(cModels[cModel], pModel[n], cModels[cModel]->pModelIdx,
         cModels[cModel]->alphaDen);
         ComputeWeightedFreqs(cModelWeight[n], pModel[n], PT);
 
-        if(cModels[cModel]->edits != 0){
+        if(cModels[cModel]->edits != 0)
+        {
           ++n;
           cModels[cModel]->SUBS.seq->buf[cModels[cModel]->SUBS.seq->idx] = sym;
           cModels[cModel]->SUBS.idx = GetPModelIdxCorr(cModels[cModel]->SUBS.
@@ -172,10 +188,10 @@ refNModels, INF *I){
           ComputePModel(cModels[cModel], pModel[n], cModels[cModel]->SUBS.idx, 
           cModels[cModel]->SUBS.eDen);
           ComputeWeightedFreqs(cModelWeight[n], pModel[n], PT);
-          }
+        }
 
         ++n;
-        }
+      }
 
       MX->sum  = MX->freqs[0] = 1 + (unsigned) (PT->freqs[0] * MX_PMODEL);
       MX->sum += MX->freqs[1] = 1 + (unsigned) (PT->freqs[1] * MX_PMODEL);
@@ -184,64 +200,53 @@ refNModels, INF *I){
 
       AESym(sym, (int *)(MX->freqs), (int) MX->sum, Writter);
       #ifdef ESTIMATE
-      if(P->estim != 0)
-        //fprintf(IAE, "%.3g\n", 2-(PModelSymbolNats(MX, sym) / M_LN2));
-        fprintf(IAE, "%.3g\n", PModelSymbolNats(MX, sym) / M_LN2);
+        if(P->estim != 0)
+          //fprintf(IAE, "%.3g\n", 2-(PModelSymbolNats(MX, sym) / M_LN2));
+          fprintf(IAE, "%.3g\n", PModelSymbolNats(MX, sym) / M_LN2);
       #endif
 
       cModelTotalWeight = 0;
-      for(n = 0 ; n < totModels ; ++n){
+      for(n = 0 ; n < totModels ; ++n)
+      {
         cModelWeight[n] = Power(cModelWeight[n], P->gamma) * (double) 
         pModel[n]->freqs[sym] / pModel[n]->sum;
         cModelTotalWeight += cModelWeight[n];
-        }
+      }
 
-      for(n = 0 ; n < P->nModels ; ++n){
-        if(cModels[n]->ref == TARGET){
+      for(n = 0 ; n < P->nModels ; ++n)
+      {
+        if(cModels[n]->ref == TARGET)
+        {
           UpdateCModelCounter(cModels[n], sym, cModels[n]->pModelIdx);
-          if(cModels[n]->ir != 0){                // REVERSE COMPLEMENTS
+          if(cModels[n]->ir != 0) // REVERSE COMPLEMENTS
+          {                
             irSym = GetPModelIdxIR(symbolBuffer+idx, cModels[n]);
             UpdateCModelCounter(cModels[n], irSym, cModels[n]->pModelIdxIR);
-            }
           }
         }
+      }
 
       for(n = 0 ; n < totModels ; ++n)
         cModelWeight[n] /= cModelTotalWeight; // RENORMALIZE THE WEIGHTS
 
-/*
-      for(cModel = 0 ; cModel < P->nModels ; ++cModel)
-        if(cModels[cModel]->ctx > 12){
-          cModels[cModel]->alphaDen = 1 + ((int)((cModelWeight[cModel]) * 100));
-          if(cModels[cModel]->edits != 0)
-            cModels[cModel]->SUBS.eDen =  1 + ((int)((cModelWeight[cModel+1]) * 10));
-          }
-*/
-/*
-
-      for(n = 0 ; n < totModels ; ++n){
-        fprintf(stdout, "%2lf\t", cModelWeight[n]);
-        }
-      fprintf(stdout, "\n");
-*/
-
-
-
       n = 0;
-      for(cModel = 0 ; cModel < P->nModels ; ++cModel){
-        if(cModels[cModel]->edits != 0){
+      for(cModel = 0 ; cModel < P->nModels ; ++cModel)
+      {
+        if(cModels[cModel]->edits != 0)
+        {
           CorrectCModelSUBS(cModels[cModel], pModel[++n], sym);
-          }
-        ++n;
         }
+        ++n;
+      }
 
-      if(++idx == BUFFER_SIZE){
+      if(++idx == BUFFER_SIZE)
+      {
         memcpy(symbolBuffer-BGUARD, symbolBuffer+idx-BGUARD, BGUARD);
         idx = 0;
-        }
+      }
 
       ++compressed;
-      }
+    }
 
   finish_encode(Writter);
   doneoutputtingbits(Writter);
@@ -249,24 +254,29 @@ refNModels, INF *I){
 
 
   #ifdef ESTIMATE
-  if(P->estim == 1){
-    fclose(IAE);
-    Free(IAEName);
+    if(P->estim == 1)
+    {
+      fclose(IAE);
+      Free(IAEName);
     }
   #endif
 
   Free(MX);
   Free(name);
   Free(cModelWeight);
+
   for(n = 0 ; n < P->nModels ; ++n)
     if(P->model[n].type == REFERENCE)
       ResetCModelIdx(cModels[n]);
     else
       FreeCModel(cModels[n]);
-  for(n = 0 ; n < totModels ; ++n){
+
+  for(n = 0 ; n < totModels ; ++n)
+  {
     Free(pModel[n]->freqs);
     Free(pModel[n]);
-    }
+  }
+
   Free(pModel);
   Free(PT);
   Free(readerBuffer);
@@ -278,14 +288,14 @@ refNModels, INF *I){
 
   I[id].bytes = _bytes_output;
   I[id].size  = compressed;
-  }
+}
 
 
 //////////////////////////////////////////////////////////////////////////////
 // - - - - - - - - - - - - - - - - R E F E R E N C E - - - - - - - - - - - - -
 
 CModel **LoadReference(Parameters *P)
-  {
+{
   FILE      *Reader = Fopen(P->ref, "r");
   uint32_t  n, k, idxPos;
   uint64_t  nBases = 0;
@@ -293,8 +303,9 @@ CModel **LoadReference(Parameters *P)
   uint8_t   *readerBuffer, *symbolBuffer, sym, irSym, type = 0, header = 1, 
             line = 0, dna = 0;
   CModel    **cModels;
+  
   #ifdef PROGRESS
-  uint64_t  i = 0;
+    uint64_t  i = 0;
   #endif
 
   if(P->verbose == 1)
@@ -310,39 +321,44 @@ CModel **LoadReference(Parameters *P)
       P->model[n].ir, REFERENCE, P->col, P->model[n].edits, P->model[n].eDen);
 
   sym = fgetc(Reader);
-  switch(sym){ 
+  switch(sym)
+  { 
     case '>': type = 1; break;
     case '@': type = 2; break;
     default : type = 0;
-    }
+  }
   rewind(Reader);
 
-  switch(type){
+  switch(type)
+  {
     case 1:  nBases = NDNASymInFasta(Reader); break;
     case 2:  nBases = NDNASymInFastq(Reader); break;
     default: nBases = NDNASyminFile (Reader); break;
-    }
+  }
 
   P->checksum = 0;
   while((k = fread(readerBuffer, 1, BUFFER_SIZE, Reader)))
-    for(idxPos = 0 ; idxPos < k ; ++idxPos){
-
+    for(idxPos = 0 ; idxPos < k ; ++idxPos)
+    {
       sym = readerBuffer[idxPos];
-      if(type == 1){  // IS A FAST[A] FILE
+      if(type == 1) // IS A FAST[A] FILE
+      {  
         if(sym == '>'){ header = 1; continue; }
         if(sym == '\n' && header == 1){ header = 0; continue; }
         if(sym == '\n') continue;
         if(header == 1) continue;
-        }
-      else if(type == 2){ // IS A FAST[Q] FILE
-        switch(line){
+      }
+      else if(type == 2)  // IS A FAST[Q] FILE
+      { 
+        switch(line)
+        {
           case 0: if(sym == '\n'){ line = 1; dna = 1; } break;
           case 1: if(sym == '\n'){ line = 2; dna = 0; } break;
           case 2: if(sym == '\n'){ line = 3; dna = 0; } break;
           case 3: if(sym == '\n'){ line = 0; dna = 0; } break;
-          }
-        if(dna == 0 || sym == '\n') continue;
         }
+        if(dna == 0 || sym == '\n') continue;
+      }
 
       // FINAL FILTERING DNA CONTENT
       if(sym != 'A' && sym != 'C' && sym != 'G' && sym != 'T')
@@ -352,23 +368,27 @@ CModel **LoadReference(Parameters *P)
       P->checksum = (P->checksum + (uint8_t) sym);
 
       for(n = 0 ; n < P->nModels ; ++n)
-        if(P->model[n].type == REFERENCE){
+        if(P->model[n].type == REFERENCE)
+        {
           GetPModelIdx(symbolBuffer+idx-1, cModels[n]);
           UpdateCModelCounter(cModels[n], sym, cModels[n]->pModelIdx);
-          if(cModels[n]->ir == 1){                         // Inverted repeats
+          if(cModels[n]->ir == 1)                        // Inverted repeats
+          { 
             irSym = GetPModelIdxIR(symbolBuffer+idx, cModels[n]);
             UpdateCModelCounter(cModels[n], irSym, cModels[n]->pModelIdxIR);
-            }
           }
+        }
 
-      if(++idx == BUFFER_SIZE){
+      if(++idx == BUFFER_SIZE)
+      {
         memcpy(symbolBuffer - BGUARD, symbolBuffer + idx - BGUARD, BGUARD);
         idx = 0;
-        }
-      #ifdef PROGRESS
-      CalcProgress(nBases, ++i);
-      #endif
       }
+
+      #ifdef PROGRESS
+        CalcProgress(nBases, ++i);
+      #endif
+    }
  
   P->checksum %= CHECKSUMGF; 
   for(n = 0 ; n < P->nModels ; ++n)
@@ -384,14 +404,15 @@ CModel **LoadReference(Parameters *P)
     fprintf(stdout, "                               \n");  // SPACES ARE VALID
 
   return cModels;
-  }
+}
   
 //////////////////////////////////////////////////////////////////////////////
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // - - - - - - - - - - - - - - - - - M A I N - - - - - - - - - - - - - - - - -
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-int32_t main(int argc, char *argv[]){
+int32_t main(int argc, char *argv[])
+{
   char        **p = *&argv, **xargv, *xpl = NULL;
   CModel      **refModels;
   int32_t     xargc = 0;
@@ -404,25 +425,29 @@ int32_t main(int argc, char *argv[]){
   INF         *I;
 
   P = (Parameters *) Malloc(1 * sizeof(Parameters));
-  if((P->help = ArgsState(DEFAULT_HELP, p, argc, "-h")) == 1 || argc < 2){
+  if((P->help = ArgsState(DEFAULT_HELP, p, argc, "-h")) == 1 || argc < 2)
+  {
     PrintMenu();
     return EXIT_SUCCESS;
-    }
+  }
 
-  if(ArgsState(DEF_VERSION, p, argc, "-V")){
+  if(ArgsState(DEF_VERSION, p, argc, "-V"))
+  {
     PrintVersion();
     return EXIT_SUCCESS;
-    }
+  }
 
-  if(ArgsState(0, p, argc, "-s")){
+  if(ArgsState(0, p, argc, "-s"))
+  {
     PrintLevels(); 
     return EXIT_SUCCESS;
-    }
+  }
 
-  if(ArgsState(DEF_EXAMPLE, p, argc, "-x")){
+  if(ArgsState(DEF_EXAMPLE, p, argc, "-x"))
+  {
     PrintExamples();
     return EXIT_SUCCESS;
-    }
+  }
 
   P->verbose  = ArgsState  (DEFAULT_VERBOSE, p, argc, "-v" );
   P->force    = ArgsState  (DEFAULT_FORCE,   p, argc, "-f" );
@@ -437,44 +462,52 @@ int32_t main(int argc, char *argv[]){
   if(P->nModels == 0 && P->level == 0)
     P->level = DEFAULT_LEVEL;
   
-  if(P->level != 0){
+  if(P->level != 0)
+  {
     xpl = GetLevels(P->level);
     xargc = StrToArgv(xpl, &xargv);
     for(n = 1 ; n < xargc ; ++n)
       if(strcmp(xargv[n], "-rm") == 0 || strcmp(xargv[n], "-tm") == 0)
         P->nModels += 1;
-    }
+  }
 
-  if(P->nModels == 0){
+  if(P->nModels == 0)
+  {
     fprintf(stderr, "Error: at least you need to use a context model!\n");
     return 1;
-    }
+  }
 
   P->model = (ModelPar *) Calloc(P->nModels, sizeof(ModelPar));
 
   k = 0;
   refNModels = 0;
   for(n = 1 ; n < argc ; ++n)
-    if(strcmp(argv[n], "-rm") == 0){
+    if(strcmp(argv[n], "-rm") == 0)
+    {
       P->model[k++] = ArgsUniqModel(argv[n+1], 1);
       ++refNModels;
-      }
-  if(P->level != 0){
+    }
+
+  if(P->level != 0)
+  {
     for(n = 1 ; n < xargc ; ++n)
-      if(strcmp(xargv[n], "-rm") == 0){
+      if(strcmp(xargv[n], "-rm") == 0)
+      {
         P->model[k++] = ArgsUniqModel(xargv[n+1], 1);
         ++refNModels;
-        }
-    }
+      }
+  }
 
   for(n = 1 ; n < argc ; ++n)
     if(strcmp(argv[n], "-tm") == 0)
       P->model[k++] = ArgsUniqModel(argv[n+1], 0);
-  if(P->level != 0){
+
+  if(P->level != 0)
+  {
     for(n = 1 ; n < xargc ; ++n)
       if(strcmp(xargv[n], "-tm") == 0)
         P->model[k++] = ArgsUniqModel(xargv[n+1], 0);
-    }
+  }
 
   gamma = DEFAULT_GAMMA;
   for(n = 1 ; n < xargc ; ++n) 
@@ -492,43 +525,48 @@ int32_t main(int argc, char *argv[]){
   P->ref      = ArgsString (NULL, p, argc, "-r");
   P->nTar     = ReadFNames (P, argv[argc-1]);
   P->checksum = 0;
+
   if(P->verbose) 
     PrintArgs(P);
 
   if(refNModels == 0)
     refModels = (CModel **) Malloc(P->nModels * sizeof(CModel *));
-  else{
-    if(P->ref == NULL){
+  else
+  {
+    if(P->ref == NULL)
+    {
       fprintf(stderr, "Error: using reference model(s) in nonexistent "
       "reference sequence!\n");
       exit(1);
-      }
+    }
     refModels = LoadReference(P);
     if(P->verbose)
       fprintf(stderr, "Checksum: %"PRIu64"\n", P->checksum);
-    }
+  }
 
   I = (INF *) Calloc(P->nTar, sizeof(INF));
 
   totalSize   = 0;
   totalBytes  = 0;
   headerBytes = 0;
-  for(n = 0 ; n < P->nTar ; ++n){
+
+  for(n = 0 ; n < P->nTar ; ++n)
+  {
     Compress(P, refModels, n, refNModels, I);
     totalSize   += I[n].size;
     totalBytes  += I[n].bytes;
     headerBytes += I[n].header;
-    }
+  }
 
   if(P->nTar > 1)
-    for(n = 0 ; n < P->nTar ; ++n){
+    for(n = 0 ; n < P->nTar ; ++n)
+    {
       fprintf(stdout, "File %d compressed bytes: %"PRIu64" (", n+1, (uint64_t) 
       I[n].bytes);
       PrintHRBytes(I[n].bytes);
       fprintf(stdout, ") , Normalized Dissimilarity Rate: %.6g\n", 
       (8.0*I[n].bytes)/(2*I[n].size));
-      }
-
+    }
 
   fprintf(stdout, "Total bytes: %"PRIu64" (", totalBytes);
   PrintHRBytes(totalBytes);
@@ -539,6 +577,4 @@ int32_t main(int argc, char *argv[]){
   fprintf(stdout, "Spent %g sec.\n", ((double)(stop-start))/CLOCKS_PER_SEC);
 
   return EXIT_SUCCESS;
-  }
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
